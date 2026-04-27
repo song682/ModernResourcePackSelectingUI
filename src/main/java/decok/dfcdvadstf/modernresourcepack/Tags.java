5bf7b7f6fafccaaf18619de084b51234e16c735d
dfdvdsf <81430928+song682@users.noreply.github.com>
2026-04-27 20:56:57 +0800

    Update Tags.java

--- src/main/java/decok/dfcdvadstf/modernresourcepack/Tags.java
+++ src/main/java/decok/dfcdvadstf/modernresourcepack/Tags.java
@@ -3,5 +3,5 @@ package decok.dfcdvadstf.modernresourcepack;
 public class Tags {
     public static final String MODID = "modernresourceselectui";
     public static final String NAME = "ModernResourcePackUI";
-    public static final String VERSION = "0.2.2";
+    public static final String VERSION = "0.3.0";
 }
